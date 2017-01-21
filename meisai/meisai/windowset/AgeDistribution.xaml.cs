c9755168f89cbe21592ff1e6978e0563b94a7b87
root <377545660@qq.com>
2017-01-21 14:12:24 +0800

    新增柱状图组件

---
+++ meisai/meisai/windowset/AgeDistribution.xaml.cs
@@ -0,0 +1,37 @@
+﻿using meisai.government;
+using meisai.windowset.basis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace meisai.windowset
+{
+    /// <summary>
+    /// Interaction logic for AgeDistribution.xaml
+    /// </summary>
+    public partial class AgeDistribution : Window
+    {
+        Government goverment;
+
+        public AgeDistribution(Government goverment_)
+        {
+            goverment = goverment_;
+            InitializeComponent();
+            //this.AddChild(histogram);
+        }
+        public void Refresh()
+        {
+            histogram.Refresh();
+        }
+    }
+}
---
+++ meisai/meisai/windowset/basis/histogram.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace meisai.windowset.basis
+{
+    /// <summary>
+    /// Interaction logic for histogram.xaml
+    /// </summary>
+    public partial class Histogram : UserControl
+    {
+        public Histogram()
+        {
+            InitializeComponent();
+        }
+        public void Refresh()
+        {
+            //可以用了
+            //MessageBox.Show("Width=" + canvas.ActualWidth);
+        }
+    }
+}
